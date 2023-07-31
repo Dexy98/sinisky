7caecc1e21e19c20fceb39e74b0c6296b4b1c69d
Dexy98 <davidecandia@live.it>
2023-07-31 16:50:22 +0200

    text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>ciao</h1>
+    <div className=" text-[50px] font-bold">
+      <h1 >ciao</h1>
+      <p>prova</p>
     </div>
   );
 }
