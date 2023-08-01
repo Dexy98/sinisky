7dd0639833c2d7b7ae4fc3841ad422d38e8ac25e
Dexy98 <davidecandia@live.it>
2023-08-01 11:50:53 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className=" text-[50px] font-bold">
-      <h1 >ciao</h1>
-      <p>prova</p>
+    <div className="">
+      <div>
+
+      <Home />
+      </div>
+    
+    <div>
+
+      <h1 className="   text-[50px]">app.js</h1>
+    </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div className="text-white">
+      <h1 className=" uppercase">home</h1>
+      <h1 className=" uppercase">service</h1>
+      <h1 className=" uppercase">contatti</h1>
+    </div>
+  );
+};
+
+export default Navbar;
