751f74bfc66136607277f77895b8f911860015c9
Dexy98 <davidecandia@live.it>
2023-08-02 16:16:13 +0200

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,35 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import logo from "../assets/logo.svg";
 import scritta from "../assets/scritta.svg";
 import { Link } from 'react-router-dom';
 import './Styles/Navbar.css';
 import { FaBars, FaTimes } from "react-icons/fa"
+import { useState } from 'react';
 
 const Navbar = () => {
-  const navRef = useRef();
+  const [click, setClick] = useState(false);
+ 
 
   const showNavBar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+    setClick(!click);
   }
 
+
   return (
-    <div className="navigation" ref={navRef}>
+    <div className="navigation" >
       <div className="logo">
         <img src={logo} alt="logo" />
         <img src={scritta} alt="scritta" />
       </div>
-      <div className="nav-links">
-        <Link to="/sinisky" className="uppercase">home</Link>
-        <Link to="/sinisky/Service" className="uppercase">service</Link>
-        <Link to="/sinisky/Contatti" className="uppercase">contatti</Link>
-      <button className='nav-btn nav-close-btn' onClick={showNavBar}>
-        <FaTimes />
-      </button>
+      <div className={click ? "nav active" : "nav"} >
+        <Link to="/sinisky" className="uppercase text-white">home</Link>
+        <Link to="/sinisky/Service" className="uppercase text-white">service</Link>
+        <Link to="/sinisky/Contatti" className="uppercase text-white">contatti</Link>
       </div>
-      <button className='nav-btn' onClick={showNavBar}>
-        <FaBars />
-      </button>
+      <div className='nav-btn text-4xl text-white' onClick={showNavBar}>
+      {click ? <FaTimes /> : <FaBars />}
+      </div>
+
     </div>
   );
 };
